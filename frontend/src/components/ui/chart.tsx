198197765a40035c0c621939a0f2bda55fbeeb9c
Thilak <thilak.s@prosperr.io>
2026-01-18 13:11:26 +0530

    meta data optimisation 2

--- frontend/src/components/ui/chart.tsx
+++ frontend/src/components/ui/chart.tsx
@@ -1,5 +1,10 @@
 /*
- * Copyright (c) 2026 Thilaks. All Rights Reserved.
+ * Copyright (c) 2026 Thilak S. All Rights Reserved.
+ *
+ * This source code, inclusive of the logic, design, and intellectual property,
+ * is the sole property of Thilak S.
+ *
+ * Created by Thilak S.
  *
  * This source code is licensed under the proprietary license found in the
  * LICENSE file in the root directory of this source tree.
